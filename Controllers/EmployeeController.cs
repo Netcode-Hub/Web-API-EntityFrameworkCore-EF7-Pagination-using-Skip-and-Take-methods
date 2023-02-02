8f39fd4b75abceed9dd5c62b528a7d374e09110e
Netcode-Hub <110794348+Netcode-Hub@users.noreply.github.com>
2023-02-02 01:27:04 +0000

    Add files via upload
    Web API EntityFrameworkCore (EF7) Pagination using Skip() and Take() methods.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using EFCoreRelationships.Data;
+using EFCoreRelationships.DTOs;
+using EFCoreRelationships.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreRelationships.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AppDbContext appDbContext;
+
+        public EmployeeController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        [HttpGet("{page}")]
+        public async Task<ActionResult<List<Employee>>> GetEmployees(int page)
+        {
+            var pageResults = 3f;
+            var pageCount = Math.Ceiling(appDbContext.Employees.Count() / pageResults);
+            var employees = await appDbContext.Employees.Skip((page - 1) * (int)pageResults).Take((int)pageResults).ToListAsync();
+
+            var response = new ResponseDTO
+            {
+                Employees = employees,
+                CurrentPage = page,
+                Page = (int)pageCount
+            };
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ResponseDTO>>> GetAllEmployees()
+        {
+            var emps =  await appDbContext.Employees.ToListAsync();
+            return Ok(emps);
+        }
+    }
+}
---
+++ DTOs/ResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using EFCoreRelationships.Models;
+
+namespace EFCoreRelationships.DTOs
+{
+    public class ResponseDTO
+    {
+        public int Page { get; set; }
+        public int CurrentPage { get; set; }
+
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+
+        
+    }
+}
